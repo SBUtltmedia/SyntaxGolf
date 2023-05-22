e372f5596cb15d54eacfed84c9ff51d914bee65f
Erin <erin.mccartney@stonybrook.edu>
2023-05-22 12:41:14 -0400

    cleaned up code by getting rid of commented out code and console.logs

--- scripts/golf.js
+++ scripts/golf.js
@@ -40,7 +40,6 @@ function makeSelectable(sentence, row, blockIndex) {
             },
 
             "touchmove": function(e) {
-                //selected(this) 
                 let elem = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
                 $(elem).trigger("touching")               
             }
@@ -56,21 +55,14 @@ function makeSelectable(sentence, row, blockIndex) {
     let blockDiv = $("<div/>",{id:blockID, "data-index":blockIndex, class:"block"}).on({
 
         mousedown: function (e) {
-            console.log($(this))
             let clickedID = $(this).attr("id")
             let selectedJQ = $(`#${clickedID} .selected`)
-            console.log(selectedJQ)
-            console.log(`#${clickedID} .selected`)
             e.stopPropagation();
             if (selectedJQ.length) {
                 let selectedWords = selectedJQ
                 selectedJQ.addClass("faded").removeClass("selected") // eventually should be hidden and/or unable to be selected again
                 let constituent = sentenceArrayToSentence(selectedWords)
                 newIndex = blockIndex + parseInt(selectedWords[0].dataset.index)
-                //$("#lineContainer").empty()
-                // $(".wordContainer").each(function(){
-                //     drawDot($(this))
-                // })
                 
                 makeSelectable(constituent, row+1, newIndex); 
                 
@@ -93,15 +85,10 @@ function makeSelectable(sentence, row, blockIndex) {
     // actually put the constituent on the page
     // prepend to first div whose index is greater
 
-    //$(`[data-row="${row}"]`).append(blockDiv); 
-    console.log($(`[data-row="${row}"]`).children().length)
     if ($(`[data-row="${row}"]`).children().length) {
         let greatest = true
         $(`[data-row="${row}"]`).children().each(function(item){
-            console.log($(this)[0].dataset.index)
-            console.log(blockIndex)
             if(blockIndex < $(this)[0].dataset.index) {
-                console.log("insert")
                 blockDiv.insertBefore($(this))
                 greatest = false
                 return false
@@ -118,11 +105,8 @@ function makeSelectable(sentence, row, blockIndex) {
 
 function selected(el) {
     let thisBlockID = $(el).parent().parent().attr("id")
-    console.log(thisBlockID)
     
     if ($(".selected").length) {
-        console.log($($(".selected")[0]))
-        console.log($($(".selected")[0]).parent().parent().attr("id"))
         selectedID = $($(".selected")[0]).parent().parent().attr("id")
         if (thisBlockID != selectedID) {
             return
@@ -145,13 +129,9 @@ function drawLines() {
     // children of foundation
     $(`${foundation}`).children().each(function(row){
         let rowThis = $(this)
-        console.log(row)
         let rowIndex = parseInt(rowThis.data("row"))
-        console.log(rowIndex)
         if (rowIndex > 0) { // skip root node           
             rowThis.children().each(function(block){
-                console.log($(this))
-                //let aboveRow = $(`[data-row="${rowIndex-1}"]`)
                 let parent = findParent($(this))
                 drawLine($(this), parent)
             })
@@ -166,15 +146,11 @@ function findParent(block) {
     // row is row before row of block, look there for parent
     let parent = -1
     rowIndex = parseInt(block.parent().data("row"))
-    console.log(rowIndex)
-    console.log(block.parent())
-    console.log(block)
     row = $(`[data-row="${rowIndex-1}"]`)
     row.children().each(function(){
         if($(this).data("index") > $(block).data("index")){
             return false
         }
-        console.log("found parent")
         parent = $(this)
     })
     return parent
@@ -182,29 +158,17 @@ function findParent(block) {
 
 function drawLine(child, parent) {
     // takes jquery items
-    console.log("drawing")
-    console.log(parent)
 
     let containerWidth = $("#lineContainer").width()
     let containerHeight = $("#lineContainer").height()
-    // let parentWidth = parent.width()
-    // let parentHeight = parent.height()
-    // let parentLeft = parent.position().left
-    // let parentTop = parent.position().top
-    // let parentCenterX = parentLeft + (parentWidth / 2)
-    // let parentCenterY = parentTop + (parentHeight / 2)
-    // let parentCenterXPercent = parentCenterX / containerWidth * 100
-    // let parentCenterYPercent = parentCenterY / containerHeight * 100
+    
     let [pleft, ptop, pright, pbottom] = getCorners(parent)
-    // console.log(getCorners(parent))
-    // console.log(pleft, ptop, pright, pbottom)
     let pCenterX = (pleft+pright) / 2
     let pCenterXPercent = pCenterX / containerWidth * 100
     let pbottomPercent = pbottom / containerHeight * 100
     // start of line will be (pCenterXPercent, pbottomPercent)
     
     let [cleft, ctop, cright, cbottom] = getCorners(child)
-    // console.log(cleft, ctop, cright, cbottom)
     let cCenterX = (cleft+cright) / 2
     let cCenterXPercent = cCenterX / containerWidth * 100
     let ctopPercent = ctop / containerHeight * 100
@@ -233,8 +197,6 @@ function drawDot(elem) {
     $("#lineContainer").append(shape);
 	$(shape).attr({cx:`${centerXPercent}%`, cy:`${centerYPercent}%`, r:"0.4%", "data-word":word});
 
-    console.log(word)
-
     // testing putting dots in upper left and bottom right
     let leftPercent = left / containerWidth * 100
     let topPercent = top / containerHeight * 100
@@ -261,34 +223,17 @@ function drawDot(elem) {
 }
 
 function getCorners(elem) {
-    // let width = elem.width()
-    // let height = elem.height()
     let width = elem[0].offsetWidth
-    console.log(width)
     let height = elem[0].offsetHeight
     let left = elem.position().left
-    console.log(left)
     let top = elem.position().top
     let right = left + width
     let bottom = top + height
-    console.log(left, top, right, bottom)
     
     return Object.values({left, top, right, bottom})
 }
 
 function generateMenu() {
-    console.log($(this))
-    
-
-    let labelTree = {items:[
-        {label:"N", items:[{label:"N'"}, {label:"NP"}]}, 
-        {label:"V", items:[{label:"V'"}, {label:"VP"}]}, 
-        {label:"P", items:[{label:"P'"}, {label:"PP"}]}, 
-        {label:"D", items:[{label:"D'"}, {label:"DP"}]}, 
-        {label:"C", items:[{label:"C'"}, {label:"CP"}]}, 
-        {label:"T", items:[{label:"T'"}, {label:"TP"}]}
-    ]} // not needed?
-    // add Adj, Adv, and S (but no S' or SP)
 
     let labels = ["N", "V", "P", "Adj", "Adv", "D", "C", "T", "S"]
 
@@ -310,15 +255,11 @@ function generateMenu() {
     $(this).find(".typeItem").on({
         "click":function(e) {
             let labelHTML = $(this).html()
-            console.log(labelHTML)
             for (symbol of Object.keys(symbolMap)) {
                 if (symbol!=labelHTML) {
-                    console.log(symbol, labelHTML)
                     $(this).parent().parent().find(".labelItem").removeClass(symbolMap[symbol])
                 }
             }
-            console.log($(this))
-            console.log($(this).parent().parent().find(".labelItem"))
             $(this).parent().parent().find(".labelItem").toggleClass(symbolMap[labelHTML])
 
         }
@@ -326,21 +267,13 @@ function generateMenu() {
 
     $(".labelItem").on({
         "click":function(e) {
-            console.log($(this).attr("class"))
-            console.log($(this).attr("class").split(" ").length)
             let classes = new Set($(this).attr("class").split(" "))
             let types = new Set(Object.values(symbolMap))
             let intersect = [...classes].filter(i => types.has(i))[0] || ""
-            console.log({intersect, classes, types})
             let symbol = inverse(symbolMap)[intersect] || ""
-            console.log(symbol)
-            console.log($(this).html())
-            console.log($(this).html() + symbol)
             let label = $(this).html() + symbol
             // replace ? with label and close menu
-            console.log($(this).parent().parent().text())
             $(this).parent().parent().text(label)
-            console.log($(this).parent().parent().text())
             $(this).parent().remove() // cannot be reopened due to .one({})
             drawLines()
 
