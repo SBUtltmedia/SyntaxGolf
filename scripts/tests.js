3dff7196276fb06a7f2e13e58e5118b2d3b131e9
Erin <erin.mccartney@stonybrook.edu>
2023-06-23 17:45:08 -0400

    updated tests to include columns

--- scripts/tests.js
+++ scripts/tests.js
@@ -9,34 +9,34 @@ let tree = treeToRows(parse(`(S (NP Mary) (VP (V had) (NP (D a) (N' (Adj little)
 // Correct Examples (should return true)
 
 // basic subtree
-let subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"NP","constituent":"Mary"},{"label":"VP","constituent":"had a little lamb"}]]')
+let subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"NP","constituent":"Mary", "column":0},{"label":"VP","constituent":"had a little lamb", "column":1}]]')
 console.log(isValid(tree, subtree) == true) 
 
 // missing words
-subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"NP","constituent":"Mary"}]]')
+subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"NP","constituent":"Mary", "column":0}]]')
 console.log(isValid(tree, subtree) == true)
-subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"NP","constituent":"Mary"},{"label":"VP","constituent":"had a little lamb"}],[{"label":"V","constituent":"had"}]]')
+subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"NP","constituent":"Mary", "column":0},{"label":"VP","constituent":"had a little lamb", "column":1}],[{"label":"V","constituent":"had", "column":1}]]')
 console.log(isValid(tree, subtree) == true)
 
 // missing labels
-subtree = JSON.parse('[[{"label":"?","constituent":"Mary had a little lamb"}],[{"label":"?","constituent":"Mary"},{"label":"?","constituent":"had a little lamb"}]]')
+subtree = JSON.parse('[[{"label":"?","constituent":"Mary had a little lamb", "column":0}],[{"label":"?","constituent":"Mary", "column":0},{"label":"?","constituent":"had a little lamb", "column":1}]]')
 console.log(isValid(tree, subtree) == true)
 
 // right heavy
-subtree = JSON.parse('[[{"label":"?","constituent":"Mary had a little lamb"}],[{"label":"?","constituent":"had a little lamb"}],[{"label":"?","constituent":"a little lamb"}],[{"label":"?","constituent":"little lamb"}],[{"label":"?","constituent":"lamb"}]]')
+subtree = JSON.parse('[[{"label":"?","constituent":"Mary had a little lamb", "column":0}],[{"label":"?","constituent":"had a little lamb", "column":1}],[{"label":"?","constituent":"a little lamb", "column":2}],[{"label":"?","constituent":"little lamb", "column":3}],[{"label":"?","constituent":"lamb", "column":4}]]')
 console.log(isValid(tree, subtree) == true)
 
 
 // Incorrect Examples (should return false)
 
 // too many divisions
-subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"?","constituent":"Mary"},{"label":"?","constituent":"had"},{"label":"?","constituent":"a little lamb"}]]')
+subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"?","constituent":"Mary", "column":0},{"label":"?","constituent":"had", "column":1},{"label":"?","constituent":"a little lamb", "column":2}]]')
 console.log(isValid(tree, subtree) == false)
 
 // wrong divisions
-subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"?","constituent":"Mary had"},{"label":"?","constituent":"a little lamb"}]]')
+subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"?","constituent":"Mary had", "column":0},{"label":"?","constituent":"a little lamb", "column":2}]]')
 console.log(isValid(tree, subtree) == false)
 
 // wrong label
-subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb"}],[{"label":"NP","constituent":"Mary"},{"label":"V","constituent":"had a little lamb"}]]')
+subtree = JSON.parse('[[{"label":"S","constituent":"Mary had a little lamb", "column":0}],[{"label":"NP","constituent":"Mary", "column":0},{"label":"V","constituent":"had a little lamb", "column":1}]]')
 console.log(isValid(tree, subtree) == false)
