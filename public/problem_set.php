39035fcc3cd1f4235f5dcbf5bf1d1597495a2efd
jessi <jessliu22to26@gmail.com>
2024-08-28 19:01:48 -0400

    data

--- public/problem_set.php
+++ public/problem_set.php
@@ -3,7 +3,7 @@ if(!$_POST){
 $id=$_GET['id'];
 if(isset($_SESSION['mail'])){
 list($user,$other)=explode("@",$_SESSION['mail']);
-$json=file_get_contents("./userData/$user/$id.json");
+$json=file_get_contents("./data/$user/$id.json");
 }
 if(!$json){
 $json=file_get_contents("./problem_sets/problem_1.json");
